02bd2e96bd102e63c7407b53abf5477c94c2a249
ellunare <ellunare16@gmail.com>
2018-01-15 22:41:13 +0200

    s6

--- server.js
+++ server.js
@@ -70,8 +70,8 @@ app.use('/api/comments', comments);
 // 	res.send('Invalid Endpoint');
 // });
 
-app.get('*', (req, res) => {
-	res.sendFile(__dirname, 'public/index.html');
+app.get('/[^\.]+$', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start Server
